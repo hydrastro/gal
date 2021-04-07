d656d199b598f63d8d5f215334c07f063746ff87
hydrastro <9256943+hydrastro@users.noreply.github.com>
2021-04-07 14:00:57 +0200

    R3 geometry functions cleanup

--- expressionReadingTest.c
+++ expressionReadingTest.c
@@ -4,7 +4,6 @@
 #include <math.h>
 #include <stdbool.h>
 #include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
--- lib/r3geometry.c
+++ lib/r3geometry.c
@@ -5,109 +5,90 @@
 
 /* code from the old side repository */
 Plane_t readPlane(Plane_t plane){
-        printf("ax + by + cz + d = 0\n");
-        printf("a: ");
-        plane.x = readFraction();
-        printf("b: ");
-        plane.y = readFraction();
-        printf("c: ");
-        plane.z = readFraction();
-        printf("d: ");
-        plane.d = readFraction();
+    printf("ax + by + cz + d = 0\n");
+    printf("a: ");
+    plane.x = readFraction();
+    printf("b: ");
+    plane.y = readFraction();
+    printf("c: ");
+    plane.z = readFraction();
+    printf("d: ");
+    plane.d = readFraction();
 
-        return plane;
+    return plane;
 }
 
 void printPlane(Plane_t plane){
-        printFraction(plane.x);
-        printf("x + ");
-        printFraction(plane.y);
-        printf("y + ");
-        printFraction(plane.z);
-        printf("z + ");
-        printFraction(plane.d);
-        printf(" = 0\n");
+    printf("(");
+    printFraction(plane.x);
+    printf(")x + (");
+    printFraction(plane.y);
+    printf(")y + (");
+    printFraction(plane.z);
+    printf(")z + ");
+    printFraction(plane.d);
+    printf(" = 0\n");
 }
 
-/* TODO: important: unify the getInvariant functions..... */
+fraction_t getLinearInvariant(int rows, fraction_t matrix[rows][rows]){
 
-fraction_t getConicCubicInvariant(fraction_t matrix[3][3]) {
-/*printMatrix(3, 3, matrix);printf("\n"); */
-
-    return getMatrixDeterminant(3, matrix);
-}
-fraction_t getConicQuadraticInvariant(fraction_t matrix[3][3]) {
-/*printMatrix(2, 2, matrix);printf("\n"); */
-    return getMatrixDeterminant(2, matrix);
-}
-fraction_t getConicLinearInvariant(fraction_t matrix[3][3]) {
-/*printMatrix(2, 2, matrix);printf("\n"); */
-
-    return getMatrixTrace(2, matrix);
+    return getMatrixTrace(rows - 1, matrix);
 }
 
-fraction_t getQuadraticQuarticInvariant(fraction_t matrix[4][4]) {
-
-    return getMatrixDeterminant(4, matrix);
-}
-fraction_t getQuadraticCubicInvariant(fraction_t matrix[4][4]) {
-
-    return getMatrixDeterminant(3, matrix);
-}
-fraction_t getQuadraticQuadraticInvariant(fraction_t matrix[4][4]) {
-    fraction_t cubeSubmatrix[3][3], squareSubmatrix[2][2], invariant;
-    getSubmatrix(3, 3, matrix, 3, 3, cubeSubmatrix);
-    getSubmatrix(2, 2, cubeSubmatrix, 2, 2, squareSubmatrix);
-    invariant = getMatrixDeterminant(2, squareSubmatrix);
-    getSubmatrix(2, 2, cubeSubmatrix, 1, 1, squareSubmatrix);
-    invariant = addFractions(invariant, getMatrixDeterminant(2, squareSubmatrix));
-    getSubmatrix(2, 2, cubeSubmatrix, 0, 0, squareSubmatrix);
-    invariant = addFractions(invariant, getMatrixDeterminant(2, squareSubmatrix));
-
+fraction_t getQuadraticInvariant(int rows, fraction_t matrix[rows][rows]){
+    int currentRowsNumber, i, j;
+    fraction_t tempDeterminant, invariant;
+    for(i = 0; i < rows; i++){
+        for(j = i + 1; j < rows; j++){
+            tempDeterminant = multiplyFractions(matrix[i][i], matrix[j][j]);
+            tempDeterminant = subtractFractions(tempDeterminant, multiplyFractions(matrix[i][j], matrix[j][i]));
+            invariant = addFractions(invariant, tempDeterminant);            
+        }
+    }
     return invariant;
 }
-fraction_t getQuadraticLinearInvariant(fraction_t matrix[4][4]) {
 
-    return getMatrixTrace(3, matrix);
+fraction_t getCubicInvariant(int rows, fraction_t matrix[rows][rows]){
+
+    return getMatrixDeterminant(3, matrix);
+}
+
+fraction_t getQuarticInvariant(int rows, fraction_t matrix[rows][rows]){
+
+    return getMatrixDeterminant(4, matrix);
 }
 
 
 /* TODO: define enum types for conics? and make a different function for the output */
-void printConicType(fraction_t matrix[3][3]) {
+void printConicType(fraction_t matrix[3][3]){
     int compairsonResult;
     fraction_t cubicInvariant, quadraticInvariant, linearInvariant, product, zeroFraction;
-    cubicInvariant = getConicCubicInvariant(matrix);
-    quadraticInvariant = getConicQuadraticInvariant(matrix);
-    linearInvariant = getConicLinearInvariant(matrix);
+    cubicInvariant = getCubicInvariant(3, matrix);
+    quadraticInvariant = getQuadraticInvariant(3, matrix);
+    linearInvariant = getLinearInvariant(3, matrix);
     zeroFraction = getFraction(0, 1);
 
-printf("\nci: ");printFraction(cubicInvariant);
-printf("\nqi: ");printFraction(quadraticInvariant);
-printf("\nli: ");printFraction(linearInvariant);
-
     compairsonResult = compareFractions(quadraticInvariant, zeroFraction);
-    /* we are not considering the case 0/0 */
-    /* we need to make sure that every fraction (0 / x) with x not equal 0, is reduced to 0/1 */
+    /* we are not considering the case 0/0; we need to make sure that every fraction (0 / x) with x not equal 0, is reduced to 0/1 */
     printf("\nConic type: ");
-    if(compareFractions(cubicInvariant, getFraction(0, 1)) == 0) {
-        if(compairsonResult > 0) {
+    if(compareFractions(cubicInvariant, getFraction(0, 1)) == 0){
+        if(compairsonResult > 0){
             printf("conic which degenerates in two secand lines, imaginary and conjugated");
-        } else if(compairsonResult < 0) {
-            printf("coniche spezzate in due rette secanti, reali distinte");
+        } else if(compairsonResult < 0){
             printf("conic which degenerates in two secant lines, real and distinct");
         } else {
             printf("conic which degenereates in two parallel lines");
         }
     } else {
-        if(compairsonResult > 0) {
+        if(compairsonResult > 0){
             product = multiplyFractions(cubicInvariant, linearInvariant);
             compairsonResult = compareFractions(product, zeroFraction);
-            if(compairsonResult > 0) {
+            if(compairsonResult > 0){
                 printf("imaginary ellipe");
             } else {
                 printf("real ellipse");
             }
-        } else if(compairsonResult < 0) {
+        } else if(compairsonResult < 0){
             printf("hyperbola");
         } else {
             printf("parabola");
@@ -116,12 +97,12 @@ printf("\nli: ");printFraction(linearInvariant);
     printf(".\n");
 }
 
-void printQuadraticType(fraction_t matrix[4][4]) {
+void printQuadraticType(fraction_t matrix[4][4]){
     int compairsonResult;
     fraction_t quarticInvariant, cubicInvariant, quadraticInvariant, linearInvariant, product, zeroFraction;
-    quarticInvariant = getQuadraticQuarticInvariant(matrix);
-    cubicInvariant = getQuadraticCubicInvariant(matrix);
-    quadraticInvariant = getQuadraticQuadraticInvariant(matrix);
-    linearInvariant = getQuadraticLinearInvariant(matrix);
+    quarticInvariant = getQuarticInvariant(4, matrix);
+    cubicInvariant = getCubicInvariant(4, matrix);
+    quadraticInvariant = getQuadraticInvariant(4, matrix);
+    linearInvariant = getLinearInvariant(4, matrix);
     zeroFraction = getFraction(0, 1);
 }
